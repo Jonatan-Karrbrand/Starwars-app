8297596df9affdd94a87362b168855394e736885
Jonatan Kärrbrand <jonatan@rivercode.se>
2024-05-19 19:59:15 +0200

    fetch movies serverside

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,17 @@
+import starWarsClient from "@/clients/starWarsClient";
 import Button from "@/components/Button";
-import useMovies from "@/hooks/useMovies";
 import AppLayout from "@/layouts/AppLayout";
+import { Movie, Movies } from "@/types";
+import { GetStaticProps } from "next";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
-export default function Home() {
-  const { movies, isLoading } = useMovies()
+type Props = {
+  movies: Movie[]
+}
 
+export default function Home({ movies }: Props) {
   const handleClearClickedMovies = () => {
     localStorage.removeItem('clicked_movies');
   }
@@ -17,32 +21,25 @@ export default function Home() {
       <h2 className="mb-5 text-xl font-medium">Movies</h2>
 
       <div className="grid gap-2 grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:md:grid-cols-6">
-        {movies && !isLoading ? (
-          movies.map((movie) => {
-            const movieId = movie.url.split('films/')[1].replace('/', '')
+        {movies.map((movie) => {
+          const movieId = movie.url.split('films/')[1].replace('/', '')
 
-            return (
-              <div>
-                <div className="aspect-[1/1.4] relative">
-                  <Link href={`/movies/${movieId}`}>
-                    <Image height={500} width={500}  className="object-cover" src={`/assets/images/movies/${movieId}.jpg`} alt={movie.title} />
-                  </Link>
-                </div>
-
-                <div className="bg-neutral-800 p-5">
-                  <Link href={`/movies/${movieId}`} className="mb-5 block">{movie.title}</Link>
-
-                  <Button href={`/movies/${movieId}`}>Read more</Button>
-                </div>
+          return (
+            <div>
+              <div className="aspect-[1/1.4] relative">
+                <Link href={`/movies/${movieId}`}>
+                  <Image height={500} width={500}  className="object-cover" src={`/assets/images/movies/${movieId}.jpg`} alt={movie.title} />
+                </Link>
               </div>
-            )
-          })
-        ) : (
-          [...Array(6)].map((element, itemIndex) => (
-            // eslint-disable-next-line react/no-array-index-key
-            <div className="bg-neutral-800 aspect-[1/1.8]"></div>
-          ))
-        )}
+
+              <div className="bg-neutral-800 p-5">
+                <Link href={`/movies/${movieId}`} className="mb-5 block">{movie.title}</Link>
+
+                <Button href={`/movies/${movieId}`}>Read more</Button>
+              </div>
+            </div>
+          )
+        })}
       </div>
 
       <button
@@ -54,3 +51,21 @@ export default function Home() {
     </AppLayout>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const result = await starWarsClient<Movies>({
+    endpoint: '/films'
+  })
+
+  if (!result.data) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: {
+      movies: result.data.results
+    },
+  };
+};
